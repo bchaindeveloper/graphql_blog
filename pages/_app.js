55d4bfdcce74648722b11883c86aee5b0fbdffe5
bchaindeveloper <bcdev3@protonmail.com>
2023-02-08 14:50:16 -0500

    Styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
+import React from 'react';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import '../styles/globals.scss';
+
+function MyApp({ Component, pageProps }) {
+  return (
+      <Component {...pageProps} />
+  );
 }
+
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import { Categories, PostCard, PostWidget } from '../components';
 
 const posts = [
   {title: 'React Testing', excerpt: 'Learn React Testing'},
@@ -13,17 +14,13 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico"></link>
       </Head>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
-         <div className='lg:col-span-8 col-span-1'>
-          {posts.map((post, index) => (
-          <div>
-            {post.title}
-            {post.excerpt}
-          </div>
-        ))}
+        <div className="lg:col-span-8 col-span-1">
+          {posts.map((post, index) => <PostCard post={post} key={post.title} />)}
         </div>
-      <div className='lg:col-span-4 col-span-1'>
-            <div className='lg:sticky relative top-8'>
-
+        <div className="lg:col-span-4 col-span-1">
+          <div className="lg:sticky relative top-8">
+            <PostWidget />
+            <Categories />
             </div>
       </div>
       </div>
