2ebfc1d66104b88ff81ec3ac6e6a03c487a7ae0e
bchaindeveloper <bcdev3@protonmail.com>
2023-02-17 18:31:38 -0500

    Post Widgit

--- services/index.js
+++ services/index.js
@@ -41,21 +41,42 @@ export const getPosts = async () => {
 
 export const getRecentPosts = async () => {
   const query = gql`
-    query GetPost Details() {
-      post(queryBy: createAt_ASC
-        test: 3
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
       ) {
         title
         featuredImage {
           url
         }
-        createAt
+        createdAt
         slug
       }
     }
-  `
-
+  `;
   const result = await request(graphqlAPI, query);
 
   return result.posts;
-}
+};
+
+export const getSimilarPosts = async (categories, slug) => {
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+      ) {
+        title
+        featuredImage {
+          url
+        }
+        createdAt
+        slug
+      }
+    }
+  `;
+  const result = await request(graphqlAPI, query, { slug, categories });
+
+  return result.posts;
+};
